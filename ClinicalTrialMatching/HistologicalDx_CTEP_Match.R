5fef20707d5cdabe76727c3b40dbc98846c7a55b
Jessica Chen <26443982+chenjwr@users.noreply.github.com>
2019-02-25 15:00:08 -0800

    Create HistologicalDx_CTEP_Match.R

---
+++ ClinicalTrialMatching/HistologicalDx_CTEP_Match.R
@@ -0,0 +1,13 @@
+# Remove NA histological dx columns
+HistologicalDxCategory <- HistologicalDxCategory[which(!is.na(HistologicalDxCategory$histologicalDiagnosis)),]
+
+# Convert all text to lowercase
+HistologicalDxCategory$histologicalDiagnosis <- tolower(HistologicalDxCategory$histologicalDiagnosis)
+HistologicalDxCategory$primaryTumorSite <- tolower(HistologicalDxCategory$primaryTumorSite)
+HistologicalDxCategory$CTEP.CATEGORY <- tolower(HistologicalDxCategory$CTEP.CATEGORY)
+HistologicalDxCategory$CTEP.SUBCATEGORY <- tolower(HistologicalDxCategory$CTEP.SUBCATEGORY)
+HistologicalDxCategory$CTEP.TERM <- tolower(HistologicalDxCategory$CTEP.TERM)
+HistologicalDxCategory$SHORT.NAME <- tolower(HistologicalDxCategory$SHORT.NAME)
+
+# Assign DF to global environment
+assign("HistologicalDxCategory", HistologicalDxCategory, envir = .GlobalEnv)
