d411c74e717fe10b5edbf2112896edc03b139e23
Jessica Chen <26443982+chenjwr@users.noreply.github.com>
2019-03-05 11:03:12 -0800

    Update Patient_Variant_Report_InclusionMatch.R

--- ClinicalTrialMatching/Patient_Variant_Report_InclusionMatch.R
+++ ClinicalTrialMatching/Patient_Variant_Report_InclusionMatch.R
@@ -1,12 +1,8 @@
-# Match by gene > variant type/function > pathogenicity.status > age.group (i.e. adult) \
-# > exclusion variant <Gene Protein Variant_Type> \
+# Match by gene > variant type > Genomic_Region > pathogenicity.status > age.group (i.e. adult) \
+# > exclusion variant <Gene Genomic_Region Variant_Type> \
 # > exclusion nonhotspot <Gene Exon_Number Variant_Type>
 ## Search Output: individual patient file with stop point
 ## Match Output: "NCIMatch_NonHotspot_Matched.tsv"
-
-# HGVSGenomic STAMP entry does not always correspond to CHR:POS_REF_ALT of NCI-MATCH criteria
-# Reason: positions are +/- 1 base pair off and/or missing information
-# NCI-MATCH Protein entries without HGVS nomenclature will be missed 
 
 if (isTRUE(NCI_match)) {
   ncol_STAMP <- as.numeric(ncol(STAMP_DF))
@@ -21,7 +17,7 @@ if (isTRUE(NCI_match)) {
   
   ## Iterate through each patient_id of patient.list
   #----------------------------------------------
-  for (patient_num in 1:length(patient.list)) {
+  for (patient_num in 1:length(patient.list)) { 
     patient_id <- patient.list[patient_num]
     
     DF_Output_Patient_Variant_int <- data.frame(matrix(NA, ncol = (ncol_STAMP + ncol_InclusionVariants)))
@@ -76,33 +72,33 @@ if (isTRUE(NCI_match)) {
           #----------------------------------------------
           if (isTRUE(var.type_id %in% var.type.trial & nrow(DF_Gene_Patient_Variant) > 0)) {
             
-            ## Parse Protein
+            ## Parse GenomicRegion
             #----------------------------------------------
-            protein.trial <- 
-              unique(DF_Gene_Patient_Variant$Protein[DF_Gene_Patient_Variant$Variant_Type == var.type_id])
-            protein.patient <- 
-              unique(DF_patient$VariantHGVSProtein[DF_patient$VariantGene == gene_id & DF_patient$var.type == var.type_id])
+            genomic.trial <- 
+              unique(DF_Gene_Patient_Variant$Genomic[DF_Gene_Patient_Variant$Variant_Type == var.type_id])
+            genomic.patient <- 
+              unique(DF_patient$VariantHGVSGenomic[DF_patient$VariantGene == gene_id & DF_patient$var.type == var.type_id])
             
-            # Remove no match Protein from DF_Gene_Patient_Variant
-            if (isTRUE(length(protein.trial) > 0)) {
-              for (pro_num in 1:length(protein.trial)) {
-                if (!(protein.trial[pro_num] %in% protein.patient)) {
-                  DF_Gene_Patient_Variant <- DF_Gene_Patient_Variant[DF_Gene_Patient_Variant$Protein !=
-                                                                       protein.trial[pro_num],]
+            # Remove no match Genomic Region from DF_Gene_Patient_Variant
+            if (isTRUE(length(genomic.trial) > 0)) {
+              for (pro_num in 1:length(genomic.trial)) {
+                if (!(genomic.trial[pro_num] %in% genomic.patient)) {
+                  DF_Gene_Patient_Variant <- DF_Gene_Patient_Variant[DF_Gene_Patient_Variant$Genomic !=
+                                                                       genomic.trial[pro_num],]
                 }}
               
               # Update list 
-              protein.trial <- unique(DF_Gene_Patient_Variant$Protein)
+              genomic.trial <- unique(DF_Gene_Patient_Variant$Genomic)
             }
             
-            ## Iterate through each protein_id of protein.patient
+            ## Iterate through each genomic_id of genomic.patient
             #----------------------------------------------
-            for (pro_num in 1:length(protein.patient)) {
-              protein_id <- protein.patient[pro_num]
+            for (gen_num in 1:length(genomic.patient)) {
+              genomic_id <- genomic.patient[gen_num]
               
-              ## Match Protein
+              ## Match Genomic Region
               #----------------------------------------------
-              if (isTRUE(protein_id %in% protein.trial & nrow(DF_Gene_Patient_Variant) > 0)) {
+              if (isTRUE(genomic_id %in% genomic.trial & nrow(DF_Gene_Patient_Variant) > 0)) {
                 
                 ## Assess Pathogenicity Status
                 #----------------------------------------------
@@ -111,7 +107,7 @@ if (isTRUE(NCI_match)) {
                 if (isTRUE(pathogenic_FILTER)) {
                   if (DF_patient$VariantPathogenicityStatus[which(DF_patient$VariantGene == gene_id &
                                                                   DF_patient$var.type == var.type_id &
-                                                                  DF_patient$VariantHGVSProtein == protein_id)] 
+                                                                  DF_patient$VariantHGVSGenomic == genomic_id)] 
                       %in% pathogenic_accepted) {
                     pathogenicity_gate <- as.logical("TRUE")
                     
@@ -119,14 +115,14 @@ if (isTRUE(NCI_match)) {
                     pathogenicity_gate <- as.logical("FALSE")
                     DF_patient$Patient_Variant_Inclusion_Status[which(DF_patient$VariantGene == gene_id &
                                                                         DF_patient$var.type == var.type_id &
-                                                                        DF_patient$VariantHGVSProtein == protein_id)] <-
+                                                                        DF_patient$VariantHGVSGenomic == genomic_id)] <-
                       "Pathogenicity criteria NOT satisfied"
                   }
                 }
                 
                 pathogenic_id <- DF_patient$VariantPathogenicityStatus[which(DF_patient$VariantGene == gene_id &
                                                                                DF_patient$var.type == var.type_id &
-                                                                               DF_patient$VariantHGVSProtein == protein_id)]
+                                                                               DF_patient$VariantHGVSGenomic == genomic_id)]
                 
                 ## Match Pathogenicity Status
                 #----------------------------------------------
@@ -144,7 +140,7 @@ if (isTRUE(NCI_match)) {
                       age_gate <- as.logical("FALSE")
                       DF_patient$Patient_Variant_Inclusion_Status[which(DF_patient$VariantGene == gene_id &
                                                                           DF_patient$var.type == var.type_id &
-                                                                          DF_patient$VariantHGVSProtein == protein_id &
+                                                                          DF_patient$VariantHGVSGenomic == genomic_id &
                                                                           DF_patient$VariantPathogenicityStatus == pathogenic_id)] <-
                         "Age criteria NOT satisfied"
                     }
@@ -157,13 +153,13 @@ if (isTRUE(NCI_match)) {
                     # Corresponding row in patient file
                     pt_rowNo <- which(DF_patient$VariantGene == gene_id &
                                         DF_patient$var.type == var.type_id &
-                                        DF_patient$VariantHGVSProtein == protein_id &
+                                        DF_patient$VariantHGVSGenomic == genomic_id &
                                         DF_patient$VariantPathogenicityStatus == pathogenic_id)
                     
                     arm.match <- unique(DF_Gene_Patient_Variant$Arm_Name
                                         [which(DF_Gene_Patient_Variant$Gene_Name == gene_id &
                                                  DF_Gene_Patient_Variant$Variant_Type == var.type_id &
-                                                 DF_Gene_Patient_Variant$Protein == protein_id)])
+                                                 DF_Gene_Patient_Variant$Genomic == genomic_id)])
                     
                     DF_patient$Patient_Variant_Inclusion_Status[pt_rowNo] <- paste("Candidate trial IDENTIFIED: ", 
                                                                                    paste(arm.match, collapse = ", "), sep="")
@@ -181,11 +177,11 @@ if (isTRUE(NCI_match)) {
                         if (nrow(DF_Exclude_Arm) > 0) {
                           
                           # All variant labels for Arm_No in Exclusion_Variants
-                          var.exclude <- sort(unique(paste(DF_Exclude_Arm$Gene_Name, " ", DF_Exclude_Arm$Protein, " (", 
+                          var.exclude <- sort(unique(paste(DF_Exclude_Arm$Gene_Name, " ", DF_Exclude_Arm$Genomic, " (", 
                                                            DF_Exclude_Arm$Variant_Type, ")", sep="")))
                           
                           # All variant labels for patient in DF_patient
-                          var.patient <- sort(unique(paste(DF_patient$VariantGene, " ", DF_patient$VariantHGVSProtein, " (",
+                          var.patient <- sort(unique(paste(DF_patient$VariantGene, " ", DF_patient$VariantHGVSGenomic, " (",
                                                            DF_patient$var.type, ")", sep="")))
                           
                           # Identify overlapping variants
@@ -343,7 +339,7 @@ if (isTRUE(NCI_match)) {
                           # Corresponding patient INFO - assume single row
                           patient_INFO <- DF_patient[which(DF_patient$VariantGene == gene_id &
                                                              DF_patient$var.type == var.type_id &
-                                                             DF_patient$VariantHGVSProtein == protein_id), c(1:ncol_STAMP)]
+                                                             DF_patient$VariantHGVSGenomic == genomic_id), c(1:ncol_STAMP)]
                           
                           if (nrow(patient_INFO) > 0) {
                             
@@ -351,7 +347,7 @@ if (isTRUE(NCI_match)) {
                             trial_INFO_matched <- DF_Gene_Patient_Variant[which(DF_Gene_Patient_Variant$Arm_Name == arm_id &
                                                                                   DF_Gene_Patient_Variant$Gene_Name == gene_id &
                                                                                   DF_Gene_Patient_Variant$Variant_Type == var.type_id &
-                                                                                  DF_Gene_Patient_Variant$Protein == protein_id),]
+                                                                                  DF_Gene_Patient_Variant$Genomic == genomic_id),]
                             
                             # Append merged patient INFO with trial INFO to Output file
                             for (row_No in 1:nrow(trial_INFO_matched)) {
@@ -377,21 +373,13 @@ if (isTRUE(NCI_match)) {
                 remove(pathogenic_id,pathogenicity_gate)
                 
               } else {
-                if (isTRUE(is.na(protein_id))) {
-                  DF_patient$Patient_Variant_Inclusion_Status[which(DF_patient$VariantGene == gene_id &
-                                                                      DF_patient$var.type == var.type_id &
-                                                                      is.na(DF_patient$VariantHGVSProtein))] <-
-                    "Protein criteria NOT satisfied due to NA entry"
-                  
-                } else {
-                  DF_patient$Patient_Variant_Inclusion_Status[which(DF_patient$VariantGene == gene_id &
-                                                                      DF_patient$var.type == var.type_id &
-                                                                      DF_patient$VariantHGVSProtein == protein_id)] <-
-                    "Protein criteria NOT satisfied"
-                }
+                DF_patient$Patient_Variant_Inclusion_Status[which(DF_patient$VariantGene == gene_id &
+                                                                    DF_patient$var.type == var.type_id &
+                                                                    DF_patient$VariantHGVSGenomic == genomic_id)] <-
+                  "Genomic region criteria NOT satisfied"
               }
             }
-            remove(pro_num,protein.patient,protein.trial,protein_id)
+            remove(pro_num,genomic.patient,genomic.trial,genomic_id)
             
           } else {
             DF_patient$Patient_Variant_Inclusion_Status[which(DF_patient$VariantGene == gene_id &
