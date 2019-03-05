6b6d9f914ca2a9a4bd9f326bcfacb651aeb205cd
Jessica Chen <26443982+chenjwr@users.noreply.github.com>
2019-03-05 11:01:46 -0800

    Update ClinicalTrial_Matching_PIPELINE.R

--- ClinicalTrialMatching/ClinicalTrial_Matching_PIPELINE.R
+++ ClinicalTrialMatching/ClinicalTrial_Matching_PIPELINE.R
@@ -13,22 +13,35 @@ library(openxlsx)
 args = commandArgs(trailingOnly=TRUE)
 # 1. Directory to save output to.
 data.root = args[1]
-# 2. Location of STAMP entries (syapse export).
+# 2. Location of STAMP entries.
 STAMP.file = args[2]
 # 3. Patient ID.
 patient.id = args[3]
-# 4. Location of OnCore Report (Stanford Internal Clinical Trials).
+# 4. Location of OnCore Report (Stanford Internal Clinical Trials). To turn off matching, set args == FALSE.
 OnCore.file = args[4]
-# 5. Location of Patient Variant Report (NCI-MATCH Clinical Trials).
+# 5. Location of Patient Variant Report (NCI-MATCH Clinical Trials). To turn off matching, set args == FALSE.
 NCI.file = args[5]
 # 6. Directory of pipeline scripts.
 script.root = args[6]
 # 7. Location of stamp_reference_transcripts.
-stamp_reference_transcripts = args[7]
-# 8. Location of most recent exons_ensembl.
-exons_ensembl = args[8]
-# 9. Location of amino acid conversion key.
-aminoAcid_conversion = args[9]
+stamp_reference.file = args[7]
+# 8. Location of exons_ensembl.
+exons_ensembl.file = args[8]
+# 9. Location of disease exclusion key.
+histoDx.key = args[9]
+
+## Customize trial output
+if (isTRUE(OnCore.file == "FALSE")) {
+  Internal_match = FALSE  
+} else {
+  Internal_match = TRUE
+}
+
+if (isTRUE(NCI.file == "FALSE")) {
+  NCI_match = FALSE  
+} else {
+  NCI_match = TRUE
+}
 
 ## Directories
 outdir = paste(data.root,"/trials/",sep="")
@@ -40,28 +53,36 @@ if (!dir.exists(tempdir)){dir.create(tempdir)}
 out.ouput = paste(outdir,patient.id,".out",sep="")
 err.output = paste(outdir,patient.id,".err",sep="")
 
-## Load files 
+## Load files and specify timestamps
 #----------------------------------------------
 STAMP_DF <- 
   read.csv(file = STAMP.file, header = TRUE, na.strings = c(""," ","<NA>","NA"), stringsAsFactors = FALSE, sep = "\t")
 
-OnCore_Biomarker_Report <- 
-  read.csv(file = OnCore.file, header = TRUE, na.strings = c("NA", ""), stringsAsFactors = FALSE, sep = ",")
+if (isTRUE(Internal_match)) {
+  OnCore_Biomarker_Report <- 
+    read.csv(file = OnCore.file, header = TRUE, na.strings = c("NA", ""), stringsAsFactors = FALSE, sep = ",")
+  
+  OnCore_Biomarker_Report_timestamp <- 
+    format(as.Date(paste(gsub("([[:digit:]]{4}[-][[:digit:]]{2})(.*$)", "\\1",sub(".*_", "", OnCore.file)), 
+                         "-01",sep="")), format= "%Y-%m")
+}
 
-PATIENT_VARIANT_REPORT <- import_list(NCI.file, setclass = "tbl")
+if (isTRUE(NCI_match)) {
+  PATIENT_VARIANT_REPORT <- import_list(NCI.file, setclass = "tbl")
+  
+  Patient_Variant_Report_timestamp <- 
+    format(as.Date(gsub("([[:digit:]]{4}[-][[:digit:]]{2}[-][[:digit:]]{2})(.*$)", "\\1",
+                        sub(".*_", "", NCI.file))), format= "%Y-%m-%d")
+}
 
 HistologicalDxCategory <- 
-  read.csv(file = "~/Documents/ClinicalDataScience_Fellowship/STAMP/2018-0223_HistologicalDx_CTEP.csv", 
-           header = TRUE, na.strings = c(""," ","NA","."), stringsAsFactors = FALSE, sep = ",")
-
-AminoAcid_Conversion <- 
-  read.csv(file = aminoAcid_conversion, header = TRUE, na.strings = c(""," "), stringsAsFactors = FALSE, sep = ",")
+  read.csv(file = histoDx.key, header = TRUE, na.strings = c(""," ","NA","."), stringsAsFactors = FALSE, sep = ",")
 
 stamp_reference_transcripts <- 
-  read.csv(file = stamp_reference_transcripts, header = TRUE, stringsAsFactors = FALSE, sep = "\t")
+  read.csv(file = stamp_reference.file, header = TRUE, stringsAsFactors = FALSE, sep = "\t")
 
 exons_ensembl <-
-  read.csv(file = exons_ensembl, header = TRUE, stringsAsFactors = FALSE, sep = "\t")
+  read.csv(file = exons_ensembl.file, header = TRUE, stringsAsFactors = FALSE, sep = "\t")
 
 ## Merge Gene-Exon Key Table
 stamp_reference_full <- left_join(stamp_reference_transcripts,exons_ensembl,
@@ -76,19 +97,6 @@ source("DiseaseGroupCategories.R")
 
 ## Histological Dx Ontology
 source("HistologicalDx_CTEP_Match.R")
-
-## Timestamp
-OnCore_Biomarker_Report_timestamp <- 
-  format(as.Date(paste(gsub("([[:digit:]]{4}[-][[:digit:]]{2})(.*$)", "\\1",sub(".*_", "", OnCore.file)), 
-                       "-01",sep="")), format= "%Y-%m")
-
-Patient_Variant_Report_timestamp <- 
-  format(as.Date(gsub("([[:digit:]]{4}[-][[:digit:]]{2}[-][[:digit:]]{2})(.*$)", "\\1",
-                      sub(".*_", "", NCI.file))), format= "%Y-%m-%d")
-
-## Customize trial output
-Internal_match = TRUE
-NCI_match = TRUE
 
 ## Default filters 
 adult.group_FILTER = FALSE # Adult = age >= 18+yo)
@@ -111,6 +119,12 @@ if (isTRUE(disease.group_FILTER & disease.site_FILTER)) { siteName <- "siteON" }
 if (isTRUE(disease.code_FILTER)) { dxName <- "histologicaldxON" } else { dxName <- "histologicaldxOFF" }
 filterName <- paste(groupName,siteName, "_", dxName, "_", pathoName, "_",  ageName, sep="")
 
+## Generate error file
+#----------------------------------------------
+sink(file = err.output, append = FALSE, split = FALSE)
+options(max.print=999999)
+sink()
+
 ## Write output to file
 #----------------------------------------------
 sink(file = out.ouput, append = FALSE, split = FALSE)
@@ -118,33 +132,68 @@ options(max.print=999999)
 
 ## Print parameters to output
 #----------------------------------------------
-cat("Patient ID:", patient.id, "\n", "\n",
-    "Stanford Internal Trial Timestamp: ", OnCore_Biomarker_Report_timestamp, "\n",
-    "\t", "FILTERs: disease group matched: ", disease.group_FILTER, "; disease site matched: ", disease.site_FILTER, "\n",
-    "NCI-MATCH Trial Timestamp: ", Patient_Variant_Report_timestamp, "\n",
-    "\t", "FILTERs: disease code matched: ", disease.code_FILTER, "\n",
-    "FILTERs: adult patients (age >= 18yo): ",adult.group_FILTER, "; pathogenic variants: ", pathogenic_FILTER, "\n", "\n",
-    "Outdirectory: ", data.root, "\n",
-    "Temporary Files within outdir: ", gsub(data.root, "", tempdir), "\n",
-    "Patient Match Results within outdir: ", gsub(data.root, "", outdir), "\n","\n")
+if (isTRUE(Internal_match & NCI_match)) {
+  cat("Patient ID:", patient.id, "\n", "\n",
+      "Stanford Internal Trial Timestamp: ", OnCore_Biomarker_Report_timestamp, "\n",
+      "\t", "FILTERs: disease group matched: ", disease.group_FILTER, "; disease site matched: ", disease.site_FILTER, "\n",
+      "NCI-MATCH Trial Timestamp: ", Patient_Variant_Report_timestamp, "\n",
+      "\t", "FILTERs: disease code matched: ", disease.code_FILTER, "\n",
+      "FILTERs: adult patients (age >= 18yo): ",adult.group_FILTER, "; pathogenic variants: ", pathogenic_FILTER, "\n", "\n",
+      "Outdirectory: ", data.root, "\n",
+      "Temporary Files within outdir: ", gsub(data.root, "", tempdir), "\n",
+      "Patient Match Results within outdir: ", gsub(data.root, "", outdir), "\n","\n")
+  
+} else if (isTRUE(Internal_match)) {
+  cat("Patient ID:", patient.id, "\n", "\n",
+      "Stanford Internal Trial Timestamp: ", OnCore_Biomarker_Report_timestamp, "\n",
+      "\t", "FILTERs: disease group matched: ", disease.group_FILTER, "; disease site matched: ", disease.site_FILTER, "\n",
+      "FILTERs: adult patients (age >= 18yo): ",adult.group_FILTER, "; pathogenic variants: ", pathogenic_FILTER, "\n", "\n",
+      "Outdirectory: ", data.root, "\n",
+      "Temporary Files within outdir: ", gsub(data.root, "", tempdir), "\n",
+      "Patient Match Results within outdir: ", gsub(data.root, "", outdir), "\n","\n")
+  
+} else if (isTRUE(NCI_match)) {
+  cat("Patient ID:", patient.id, "\n", "\n",
+      "NCI-MATCH Trial Timestamp: ", Patient_Variant_Report_timestamp, "\n",
+      "\t", "FILTERs: disease code matched: ", disease.code_FILTER, "\n",
+      "FILTERs: adult patients (age >= 18yo): ",adult.group_FILTER, "; pathogenic variants: ", pathogenic_FILTER, "\n", "\n",
+      "Outdirectory: ", data.root, "\n",
+      "Temporary Files within outdir: ", gsub(data.root, "", tempdir), "\n",
+      "Patient Match Results within outdir: ", gsub(data.root, "", outdir), "\n","\n")
+  
+} else {
+  sink(file = err.output, append = TRUE, split = FALSE)
+  options(max.print=999999)
+  
+  cat("NO TRIAL has been specified to be matched. Check parameter specification.", "\n","\n")
+  
+  sink()
+  
+  sink(file = out.ouput, append = TRUE, split = FALSE)
+  options(max.print=999999)
+  
+}
 
 ## PIPELINE
 #----------------------------------------------
-# Clean up patient data from Syapse
+# Clean up patient data
 source("Patient_Export_QC.R")
 
 # Extract patient_id
 patient.list <- sort(unique(STAMP_DF$PatientID))
 
 if (length(patient.list) > 0) {
-  # Clean up Clinical Trial data
-  source("Biomarker_Report_QC.R")
-  source("Patient_Variant_Report_QC.R")
   
-  # Clinical trial Match
-  source("Biomarker_Report_Match.R") 
-  source("Patient_Variant_Report_InclusionMatch.R")
-  source("Patient_Variant_Report_NonHotspotMatch.R")
+  if (isTRUE(Internal_match)) {
+    source("Biomarker_Report_QC.R")
+    source("Biomarker_Report_Match.R") 
+  }
+  
+  if (isTRUE(NCI_match)) {
+    source("Patient_Variant_Report_QC.R")
+    source("Patient_Variant_Report_InclusionMatch.R")
+    source("Patient_Variant_Report_NonHotspotMatch.R")
+  }
   
   ## Generate OUTPUT file 
   source("ClinicalTrial_MatchOutput.R")
