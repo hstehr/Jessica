e44bc67ccf9cd9b14345677db36fc7ec9f6d0460
Jessica Chen <26443982+chenjwr@users.noreply.github.com>
2019-03-05 13:19:45 -0800

    Update ClinicalTrial_Matching.sh

--- ClinicalTrialMatching/ClinicalTrial_Matching.sh
+++ ClinicalTrialMatching/ClinicalTrial_Matching.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Directories
-script_root="/Users/jessicachen/Documents/ClinicalDataScience_Fellowship/ClinicalTrialMatching"
-data_root="/Users/jessicachen/Documents/ClinicalDataScience_Fellowship/STAMP_v2.4_reports"
+data_root="/Users/jessicachen/STAMP_v2.4_reports"
+script_root="/Users/jessicachen/ClinicalTrialMatching"
 
 # Reference files
 stamp_reference=${script_root}/Ensembl-Gene-Exon-Annotations/stamp_reference_transcripts.txt
@@ -10,8 +10,8 @@ exons_ensembl=${script_root}/Ensembl-Gene-Exon-Annotations/exons_ensembl75.txt
 histoDx_key=${script_root}/HistologicalDx_CTEP.csv
 
 # Clinical trials 
-OnCore="FALSE"
-NCI=${script_root}/PATIENT_VARIANT_REPORT_TEMPLATE_YYYY-MM-DD.xlsx
+OnCore=${script_root}/Biomarker_Report_2018-10.csv
+NCI=${script_root}/PATIENT_VARIANT_REPORT_TEMPLATE_2019-02-25.xlsx
 
 # Extract STAMP sequence files and patient ID
 ls ${data_root}/reports/*.variant_report.txt | awk -F"/" '{print $NF}' > ${data_root}/trial_names.tsv
@@ -22,8 +22,7 @@ do
 	STAMP=$(cat ${lookup} | awk -F',' -v row=$i '(NR==row){print $0}')
 	patient=$(cat ${lookup} | awk -F',' -v row=$i '(NR==row){print $0}' | cut -f1 -d ".")
 
- 	echo $patient
- 	Rscript ClinicalTrial_Matching_PIPELINE.R $data_root ${data_root}/reports/$STAMP $patient $OnCore $NCI $script_root $stamp_reference $exons_ensembl $histoDx_key
+ 	Rscript ${script_root}/ClinicalTrial_Matching_PIPELINE.R $data_root $data_root/reports/$STAMP $patient $OnCore $NCI $script_root $stamp_reference $exons_ensembl $histoDx_key
 done
 
 rm ${data_root}/trial_names.tsv
