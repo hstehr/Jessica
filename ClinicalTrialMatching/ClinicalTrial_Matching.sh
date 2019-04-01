619cf7e63963c93320e77c8f0fe67a25ef23ac89
Jessica Chen <26443982+chenjwr@users.noreply.github.com>
2019-03-31 22:29:56 -0700

    Update ClinicalTrial_Matching.sh

--- ClinicalTrialMatching/ClinicalTrial_Matching.sh
+++ ClinicalTrialMatching/ClinicalTrial_Matching.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
 # Directories
-data_root="/Users/jessicachen/STAMP_v2.4_reports"
-script_root="/Users/jessicachen/scripts"
+data_root="/Users/jessicachen/Documents/ClinicalDataScience_Fellowship/STAMP_v2.4_reports"
+script_root="/Users/jessicachen/Documents/ClinicalDataScience_Fellowship/ClinicalTrialMatching/PIPELINE_scripts"
 outdir=${data_root}/trials
+
+# Clinical trials: to turn off matching, set args == "FALSE"
+OnCore=${script_root}/Biomarker_Report_2019-03.csv
+NCI=${script_root}/PATIENT_VARIANT_REPORT_TEMPLATE_2019-03-25.xlsx
 
 # Reference files
 stamp_reference=${script_root}/Ensembl-Gene-Exon-Annotations/stamp_reference_transcripts.txt
 exons_ensembl=${script_root}/Ensembl-Gene-Exon-Annotations/exons_ensembl75.txt
 histoDx_key=${script_root}/HistologicalDx_CTEP.csv
 
-# Clinical trials 
-OnCore=${script_root}/Biomarker_Report_2018-10.csv
-NCI=${script_root}/PATIENT_VARIANT_REPORT_TEMPLATE_2019-02-25.xlsx
-
 # Extract STAMP sequence files and patient ID
 ls ${data_root}/reports/*.variant_report.txt | awk -F"/" '{print $NF}' > ${data_root}/trial_names.tsv
 while read STAMP
 do
 	patient=$(basename $STAMP .variant_report.txt)
+	CNV=${patient}.cnvs
+	Fusion=${patient}.fusions.filtered.txt 
 
 	echo $patient
- 	Rscript ${script_root}/ClinicalTrial_Matching_PIPELINE.R $data_root $data_root/reports/$STAMP $patient $OnCore $NCI $script_root $outdir $stamp_reference $exons_ensembl $histoDx_key
+
+ 	Rscript ${script_root}/ClinicalTrial_Matching_PIPELINE.R $data_root \
+ 	$patient $data_root/reports/$STAMP $data_root/reports/$CNV $data_root/reports/$Fusion \
+ 	$OnCore $NCI \
+ 	$script_root $outdir \
+ 	$stamp_reference $exons_ensembl $histoDx_key
+
 done < ${data_root}/trial_names.tsv  
 
 rm ${data_root}/trial_names.tsv
