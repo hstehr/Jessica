c0ed9c619c853e0ee53927e342ccf96213486f62
Jessica Chen <26443982+chenjwr@users.noreply.github.com>
2019-04-15 21:24:14 -0700

    Update ClinicalTrial_Matching.sh

--- ClinicalTrialMatching/ClinicalTrial_Matching.sh
+++ ClinicalTrialMatching/ClinicalTrial_Matching.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Directories
-data_root="/Users/jessicachen/Documents/ClinicalDataScience_Fellowship/STAMP_v2.4_reports"
-script_root="/Users/jessicachen/Documents/ClinicalDataScience_Fellowship/ClinicalTrialMatching/PIPELINE_scripts"
+data_root="/Users/STAMP_v2.4_reports"
+script_root="/Users/PIPELINE_scripts"
 outdir=${data_root}/trials
 
 # Clinical trials: to turn off matching, set args == "FALSE"
 OnCore=${script_root}/Biomarker_Report_2019-03.csv
 NCI=${script_root}/PATIENT_VARIANT_REPORT_TEMPLATE_2019-03-25.xlsx
+# Remove NCI-MATCH ARMS: to indicate none, set args == "NULL"
+NCI_ArmRemove="ARM-F,ARM-G,ARM-K1,ARM-M,ARM-S2,ARM-Z1C,ARM-Z1F"
 
 # Reference files
 stamp_reference=${script_root}/Ensembl-Gene-Exon-Annotations/stamp_reference_transcripts.txt
@@ -26,7 +28,7 @@ do
 
  	Rscript ${script_root}/ClinicalTrial_Matching_PIPELINE.R $data_root \
  	$patient $data_root/reports/$STAMP $data_root/reports/$CNV $data_root/reports/$Fusion \
- 	$OnCore $NCI \
+ 	$OnCore $NCI $NCI_ArmRemove \
  	$script_root $outdir \
  	$stamp_reference $exons_ensembl $histoDx_key
 
