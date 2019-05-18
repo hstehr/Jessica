652b49adb63791618dc07a98dd89d0c88453853d
Jessica Chen <26443982+chenjwr@users.noreply.github.com>
2019-05-19 05:04:40 +0800

    Update Syapse_Visualizations.R

--- SyapseExport_RetrospectiveAnalysis/Syapse_Visualizations.R
+++ SyapseExport_RetrospectiveAnalysis/Syapse_Visualizations.R
@@ -19,59 +19,63 @@ if (isTRUE(static.plots_FILTER)) {
   # Remove duplicate entries 
   DF_subset <- DF_subset %>% dplyr::distinct(patient.id, .keep_all = TRUE)
   
-  # Generate histogram
-  #----------------------------------------------
-  plot <- ggplot(DF_subset, aes(DF_subset$age, fill=DF_subset$gender)) +
-    geom_histogram(bins = (max(DF_subset$age) - min(DF_subset$age) +1), col="gray") +
-    
-    labs(title = "STAMP Database: Age Distribution",
-         subtitle = paste("N = ", nrow(DF_subset), sep="")) +
-    
-    scale_x_continuous(name="Current Age", breaks = seq(0, 100, 5)) +
-    scale_y_continuous(name="Number of Samples", breaks = seq(0,150,10)) + 
-    scale_fill_discrete(name = "Gender") +
-    
-    theme_bw() +
-    theme(plot.title = element_text(hjust=0.5, face="bold",size=20),
-          plot.subtitle = element_text(hjust=1, face="bold",size=15),
-          
-          legend.background = 
-            element_rect(color = "black", fill = "white", size = 0.3, linetype = "solid"),
-          legend.position = c(0.955, 0.88),
-          legend.text=element_text(size=10),
-          
-          axis.text=element_text(size=20),
-          axis.title=element_text(size=20,face="bold"),
-          
-          plot.margin = margin(1, 1, 1, 1, "cm")) +
-    guides(fill=guide_legend(nrow=2,byrow=TRUE))
+  DF_subset <- unique(DF_subset[,c("patient.id","age","gender")])
+  colnames(DF_subset) <- c("PatientID","PatientAge","PatientGender")
   
-  # Generate output table
+  # TABLE
   #----------------------------------------------
+  # Tabulate frequency = age of patient
+  DF_tabulate_full <- data.frame(DF_subset %>% group_by(PatientAge,PatientGender) %>% tally())
+  colnames(DF_tabulate_full) <- c("PatientAge","Gender","No.Orders")
+  row_append <- data.frame(PatientAge=setdiff(seq(1,100), unique(DF_tabulate_full$PatientAge)),
+                           Gender=unique(DF_tabulate_full$Gender[[1]]),
+                           No.Orders=0, stringsAsFactors = FALSE)
+  DF_tabulate_full <- rbind(DF_tabulate_full,row_append)
+  DF_tabulate_full <- DF_tabulate_full[order(DF_tabulate_full$PatientAge, decreasing = FALSE),]
+  
+  gender.missing <- setdiff(c("Female","Male"), unique(DF_tabulate_full$Gender))
+  if (length(gender.missing) > 0) {
+    DF_tabulate_full <- rbind(DF_tabulate_full,
+                              data.frame(PatientAge=50, Gender=gender.missing,No.Orders=0, stringsAsFactors = FALSE))
+  }
+  
   # Specify Age.Cohort
   DF_subset$Age.Cohort <- NA
   for (row_No in 1:nrow(DF_subset)) {
-    if (isTRUE(DF_subset$age[row_No] < 18)) {
-      DF_subset$Age.Cohort[row_No] <- "Child (< 18yo)"
-    } else if (isTRUE(DF_subset$age[row_No] < 65)) {
-      DF_subset$Age.Cohort[row_No] <- "Adult (18-64yo)"
-    } else {
-      DF_subset$Age.Cohort[row_No] <- "Older Adult (>= 65yo)"
+    if (isTRUE(DF_subset$PatientAge[row_No] < 18)) {DF_subset$Age.Cohort[row_No] <- "Child (< 18yo)"
+    } else if (isTRUE(DF_subset$PatientAge[row_No] < 65)) {DF_subset$Age.Cohort[row_No] <- "Adult (18-64yo)"
+    } else {DF_subset$Age.Cohort[row_No] <- "Older Adult (>= 65yo)"
     }
   }
   
-  # Tabulate frequency
-  DF_subset_tabulate <- data.frame(DF_subset %>% group_by(Age.Cohort,gender) %>% tally())
-  colnames(DF_subset_tabulate) <- c("Age.Cohort","Gender","No.Samples")
-  
+  # Tabulate frequency = age cohorts
+  DF_tabulate <- data.frame(DF_subset %>% group_by(Age.Cohort,PatientGender) %>% tally())
+  colnames(DF_tabulate) <- c("Age.Cohort","Gender","No.Orders")
   # Convert table to wide format
-  DF_subset_tabulate <- data.frame(cast(DF_subset_tabulate, Age.Cohort ~ Gender),
-                                   stringsAsFactors = FALSE)
-  DF_subset_tabulate$Total <- DF_subset_tabulate$Female + DF_subset_tabulate$Male
+  DF_tabulate <- data.frame(cast(DF_tabulate, Age.Cohort ~ Gender), stringsAsFactors = FALSE)
   
-  DF_subset_tabulate <- DF_subset_tabulate[order(DF_subset_tabulate$Total),]
+  if (isTRUE(!("Male" %in% colnames(DF_tabulate)))) {DF_tabulate$Male <- as.numeric("0")}
+  if (isTRUE(!("Female" %in% colnames(DF_tabulate)))) {DF_tabulate$Female <- as.numeric("0")} 
+  DF_tabulate$Total <- DF_tabulate$Female + DF_tabulate$Male
   
-  Output.table <- tableGrob(DF_subset_tabulate, rows = NULL,
+  DF_tabulate <- DF_tabulate[,c("Age.Cohort","Female","Male","Total")]
+  DF_tabulate$Female[which(is.na(DF_tabulate$Female))] <- 0
+  DF_tabulate$Male[which(is.na(DF_tabulate$Male))] <- 0
+  DF_tabulate$Total[which(is.na(DF_tabulate$Total))] <- 0
+  
+  cohort.missing <- setdiff(c("Adult (18-64yo)","Child (< 18yo)","Older Adult (>= 65yo)"),
+                            unique(DF_tabulate$Age.Cohort))
+  if (length(cohort.missing) > 0) {
+    DF_tabulate <- rbind(DF_tabulate,
+                         data.frame(Age.Cohort=cohort.missing,
+                                    Female=0, Male=0, Total=0, stringsAsFactors = FALSE))  
+  }
+  
+  DF_tabulate$Age.Cohort <- factor(DF_tabulate$Age.Cohort,
+                                   levels = c("Child (< 18yo)","Adult (18-64yo)","Older Adult (>= 65yo)"))
+  DF_tabulate <- DF_tabulate[order(DF_tabulate$Age.Cohort),]
+  
+  Output.table <- tableGrob(DF_tabulate, rows = NULL,
                             theme = ttheme_default(core=list(fg_params=list(hjust=0, x=0.1)),
                                                    rowhead=list(fg_params=list(hjust=0, x=0)),
                                                    base_size = 10))
@@ -82,6 +86,73 @@ if (isTRUE(static.plots_FILTER)) {
                                   grobs = rectGrob(gp = gpar(fill = NA, lwd = 2)),
                                   t = 1, l = 1, r = ncol(Output.table))
   
+  # Convert to relative frequency
+  total.count = sum(DF_tabulate_full$No.Orders)
+  DF_tabulate_full$Relative.Frequency <- as.numeric(round((100 * DF_tabulate_full$No.Orders) / total.count,2))
+  
+  # Y-axis parameters
+  ymax <- c()
+  for (row_No in 1:length(seq(0,100))) {
+    ymax <- append(ymax, 
+                   sum(DF_tabulate_full$Relative.Frequency[which(DF_tabulate_full$PatientAge == seq(0,100)[row_No])]))
+  }
+  ymax <- ceiling(max(ymax)/5)*5
+  if (isTRUE(ymax <= 30)) {
+    if (isTRUE(ymax <= 20)) {y_increment = 1
+    } else {y_increment = 2
+    }
+  } else {y_increment = 5
+  }
+  
+  # HISTOGRAM
+  #----------------------------------------------
+  # Address age >90yo is PHI
+  DF_Keep <- DF_tabulate_full[which(DF_tabulate_full$PatientAge < 90),]
+  DF_Keep$PatientAge <- formatC(DF_Keep$PatientAge,0,format="f")
+  
+  DF_Edit <- DF_tabulate_full[which(DF_tabulate_full$PatientAge >= 90),]
+  DF_Edit_Summary <- data.frame()
+  gender.list <-c("Female","Male")
+  for (elem_No in 1:length(gender.list)) {
+    gender_id = gender.list[elem_No]
+    
+    DF_Edit_Summary <- rbind(DF_Edit_Summary,
+                             data.frame(PatientAge="90+",
+                                        Gender=gender_id,
+                                        No.Orders=sum(DF_Edit$No.Orders[which(DF_Edit$Gender==gender_id)]),
+                                        Relative.Frequency=sum(DF_Edit$Relative.Frequency[which(DF_Edit$Gender==gender_id)]),
+                                        stringsAsFactors = FALSE))
+  }
+  
+  DF_tabulate_full_edit <- rbind(DF_Keep,DF_Edit_Summary)
+  DF_tabulate_full_edit$PatientAge <- factor(DF_tabulate_full_edit$PatientAge,
+                                             levels=c(formatC(seq(0,89,1),0,format="f"),"90+"))
+  
+  plot <- ggplot(DF_tabulate_full_edit, aes(x=PatientAge, y=Relative.Frequency, fill=Gender)) +
+    geom_bar(stat="identity") +
+    
+    labs(title = "Age and Gender Distribution",
+         subtitle = paste("N = ", nrow(DF_subset), " ", comment, sep="")) +
+    
+    scale_x_discrete(name="Age",breaks=c(as.character(seq(0,89,5)),"90+")) +
+    scale_y_continuous(name="Percent of Test Orders", breaks = seq(0,ymax,y_increment), limits=c(0, ymax)) +
+    scale_fill_discrete(name = "Gender") +
+    
+    theme_bw() +
+    theme(plot.title = element_text(hjust=0.5, face="bold",size=20),
+          plot.subtitle = element_text(hjust=1, face="bold",size=14),
+          
+          legend.background = 
+            element_rect(color = "black", fill = "white", size = 0.3, linetype = "solid"),
+          legend.position = c(0.955, 0.88),
+          legend.text=element_text(size=10),
+          
+          axis.text.x=element_text(size=14,angle = 0),
+          axis.text.y=element_text(size=14),
+          axis.title=element_text(size=14,face="bold")) +
+    
+    guides(fill=guide_legend(nrow=2,byrow=TRUE))
+
   # Save to local computer
   #----------------------------------------------
   tiff(filename = paste(outdir, Syapse_Export_timestamp,"_syapse_GenderAgeDistribution_",
@@ -91,7 +162,7 @@ if (isTRUE(static.plots_FILTER)) {
                heights = c(2, 0.4), ncol = 1, nrow = 2)
   dev.off()
   
-  remove(DF_subset,DF_subset_tabulate,Output.table,plot,row_No)
+  remove(DF_subset,Output.table,plot,row_No)
 } 
 
 ## STAMP database: pathogenicity status + variant type
